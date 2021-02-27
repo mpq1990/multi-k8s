a40d3463b2a4eefc7ecd0389e4a56bff590bb75c
Majid Qureshi <mpq1990@gmail.com>
2021-02-27 20:47:44 +0100

    feat: feature test

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>This is my Fib Calculator</h1>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Other Page</Link>
         </header>
